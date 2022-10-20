1a6c4f2e9a3aeacab577ec7f06c2ed1a5af231b9
Matheus Henrique <matheus.rico17@gmail.com>
2022-10-20 01:06:39 -0300

    Aprendendo sobre compartilhamento de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Jeniffer';
+
+  userData = {
+    email: 'jeniffer@email.com',
+    role: 'Admin'
+  }
+
   title = 'curso-angular';
 }
