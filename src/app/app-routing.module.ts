03e2719a3a6bfcaac507d8833810685c1395f989
Matheus Henrique <matheus.rico17@gmail.com>
2022-10-23 14:47:26 -0300

    adicionando a rota de eventos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { EventosComponent } from "./components/eventos/eventos.component";
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { IfRenderComponent } from "./components/if-render/if-render.component";
 import { ListRenderComponent } from "./components/list-render/list-render.component";
@@ -9,7 +10,8 @@ const routes: Routes = [
     {path: '', component: FirstComponentComponent},
     {path: 'list', component: ListRenderComponent},
     {path: 'if', component: IfRenderComponent},
-    {path: 'twb', component: TwoWayBindingComponent}
+    {path: 'twb', component: TwoWayBindingComponent},
+    {path: 'eventos', component: EventosComponent}
 ]
 
 @NgModule({
